8370dcd33ceed2fc656d4ff95a412c7df345329b
Le Nain <pa-roussel@hotmail.fr>
2025-04-15 14:32:21 +0200

    Squelette HTML

---
+++ script.js
@@ -0,0 +1,14 @@
+// création de formulaire de connexion au clic sur le bouton "login"
+
+document.querySelector('#login').addEventListener('click', function(){
+  // ID, mdp  
+})
+
+// création de formulaire d'inscription pour un nouveau joueur
+
+document.querySelector('#signin').addEventListener('click', function(){
+    fieldset // (ID, adresse courriel, mdp)
+})
+
+
+
