75da4d34526e9aee275d2eec7090f390bf9e0004
Le Nain <pa-roussel@hotmail.fr>
2025-04-16 10:12:44 +0200

    Modif CSS

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 document.querySelector('#login').addEventListener('click', function(){
   // ID, mdp  lien vers la page de connexion
+    window.location.href = "connexion.html";
 })
 
 // lien vers le formulaire d'inscription pour un nouveau joueur
-
 document.querySelector('#signin').addEventListener('click', function(){
-    fieldset // (ID, adresse courriel, mdp)
+    window.location.href="inscription.html"
 })
 
 
