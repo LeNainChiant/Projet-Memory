9d21bb5be50c8bf99ab3d7fb23c62f53a37105ad
Le Nain <pa-roussel@hotmail.fr>
2025-04-16 09:41:05 +0200

    Ajout "profil" untracked

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// création de formulaire de connexion au clic sur le bouton "login"
+// lien vers le formulaire de connexion au clic sur le bouton "login"
 
 document.querySelector('#login').addEventListener('click', function(){
-  // ID, mdp  
+  // ID, mdp  lien vers la page de connexion
 })
 
-// création de formulaire d'inscription pour un nouveau joueur
+// lien vers le formulaire d'inscription pour un nouveau joueur
 
 document.querySelector('#signin').addEventListener('click', function(){
     fieldset // (ID, adresse courriel, mdp)
